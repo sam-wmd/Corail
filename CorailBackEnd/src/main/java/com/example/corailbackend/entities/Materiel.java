acce9a7e1a0c88b9fdfd71eb8ae0ec250b45e70a
Sam <matemusami@gmail.com>
2021-07-06 17:35:51 +0200

    First Commit Sam ---- Compte Rendu + Projet + Session

---
+++ CorailBackEnd/src/main/java/com/example/corailbackend/entities/Materiel.java
@@ -0,0 +1,2 @@
+package com.example.corailbackend.entities;public class Materiel {
+}
---
+++ CorailBackEnd/src/main/java/com/example/corailbackend/enums/EtatProjetEnum.java
@@ -0,0 +1,4 @@
+package com.example.corailbackend.enums;
+
+public enum EtatProjet { EN_COURS,BLOQUE,ARCHIVE
+}
