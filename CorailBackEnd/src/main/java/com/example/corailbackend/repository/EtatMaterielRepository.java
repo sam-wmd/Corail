99cc7b3a34cfb89d267a662bb16207e4744652b9
Ronan <ronan.poinsignon@laposte.net>
2021-07-06 16:40:03 +0200

    prémunir la création d'états en base

--- CorailBackEnd/src/main/java/com/example/corailbackend/repository/EtatMaterielRepository.java
+++ CorailBackEnd/src/main/java/com/example/corailbackend/repository/EtatMaterielRepository.java
@@ -1,7 +1,53 @@
 package com.example.corailbackend.repository;
 
+import java.util.List;
+
 import com.example.corailbackend.entity.materiel.etatmateriel.EtatMateriel;
 
 public interface EtatMaterielRepository extends AbstractEntityRepository<EtatMateriel> {
 
+	@Override
+	default <S extends EtatMateriel>  S save(S etat) {
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default <T extends EtatMateriel> List<T> saveAll(Iterable<T> entities){
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default <S extends EtatMateriel> S saveAndFlush(S entity) {
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default <S extends EtatMateriel> List<S> saveAllAndFlush(Iterable<S> entities) {
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default void delete(EtatMateriel etat) {
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default void deleteAll() {
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default void deleteAll(Iterable<? extends EtatMateriel> entities) {
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default void deleteAllById(Iterable<? extends Integer> ids) {
+		throw new IllegalAccessError();
+	}
+
+	@Override
+	default void deleteAllInBatch(Iterable<EtatMateriel> entities) {
+		throw new IllegalAccessError();
+	}
 }
