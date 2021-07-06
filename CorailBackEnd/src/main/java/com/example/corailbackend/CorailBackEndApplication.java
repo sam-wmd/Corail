a5851cf3c9f94938477ac8e9235b9ed374f53108
Heynderickx Alexandre <heynderickxalexandre@gmail.com>
2021-07-06 09:31:38 +0200

    Init commit

---
+++ CorailBackEnd/src/main/java/com/example/corailbackend/CorailBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.corailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorailBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorailBackEndApplication.class, args);
+    }
+
+}
---
+++ CorailBackEnd/src/test/java/com/example/corailbackend/CorailBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorailBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
