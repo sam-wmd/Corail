cfc28234dc024f70af0d68f16318cec599b276fb
Ronan <ronan.poinsignon@laposte.net>
2021-07-07 12:54:18 +0200

    ajout d'une méthode de récupération d'un utilisateur depuis son username

--- CorailBackEnd/src/main/java/com/example/corailbackend/repository/UserRepository.java
+++ CorailBackEnd/src/main/java/com/example/corailbackend/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package com.example.corailbackend.repository;
 
-import com.example.corailbackend.entity.user.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.persistence.Column;
-import java.util.Set;
+import com.example.corailbackend.entity.user.User;
 
 public interface UserRepository extends AbstractEntityRepository<User> {
 
-    @Query("select u.lastname, u.name, u.email, u.username, u.roles from User as u where u.id = ?1")
-    User getUserById(@Param("id") int id);
+	@Query("select u.lastname, u.name, u.email, u.username, u.roles from User as u where u.id = ?1")
+	User getUserById(@Param("id") int id);
+
+	User findByUsername(String username);
 }
