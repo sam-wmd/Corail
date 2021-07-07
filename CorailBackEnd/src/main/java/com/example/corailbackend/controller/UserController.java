0c07e40b8957ddc33c1501827a18dd2de08d82c0
Ronan <ronan.poinsignon@laposte.net>
2021-07-07 12:52:32 +0200

    modification des retours des fonctions

--- CorailBackEnd/src/main/java/com/example/corailbackend/controller/UserController.java
+++ CorailBackEnd/src/main/java/com/example/corailbackend/controller/UserController.java
@@ -1,39 +1,39 @@
 package com.example.corailbackend.controller;
 
-import com.example.corailbackend.repository.UserRepository;
-import com.example.corailbackend.entity.user.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.corailbackend.entity.user.User;
+import com.example.corailbackend.entity.user.UserDao;
+import com.example.corailbackend.repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @PostMapping("/")
-    public String createUser(@RequestBody User user) {
-        userRepository.save(user);
-        return "{ \"status\": \"Ok\" }";
-    }
+	@GetMapping
+	public List<UserDao> getUsers() {
+		return userRepository.findAll().stream().map(UserDao::new).collect(Collectors.toList());
+	}
 
-    @GetMapping("/")
-    public List<User> getUsers() {
-        return userRepository.findAll();
-    }
+	@GetMapping("/{id}")
+	public User getUser(@PathVariable int id) {
+		return new UserDao(userRepository.getUserById(id));
+	}
 
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable int id) {
-        return userRepository.getUserById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    public String removeUser(@PathVariable int id) {
-        userRepository.deleteById(id);
-        return "{ \"status\": \"Ok\" }";
-    }
+	@DeleteMapping("/{id}")
+	public String removeUser(@PathVariable int id) {
+		userRepository.deleteById(id);
+		return "{ \"status\": \"Ok\" }";
+	}
 }
