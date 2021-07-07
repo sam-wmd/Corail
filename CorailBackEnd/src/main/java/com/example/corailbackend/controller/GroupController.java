41e91bb4a0fcfb15057f8adf249a9cfef3f2f216
Ronan <ronan.poinsignon@laposte.net>
2021-07-07 12:48:48 +0200

    suppression imports inutiles

--- CorailBackEnd/src/main/java/com/example/corailbackend/controller/GroupController.java
+++ CorailBackEnd/src/main/java/com/example/corailbackend/controller/GroupController.java
@@ -1,33 +1,38 @@
 package com.example.corailbackend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.corailbackend.entity.group.Groupe;
 import com.example.corailbackend.repository.GroupeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/groups")
 public class GroupController {
 
-    @Autowired
-    private GroupeRepository groupeRepository;
+	@Autowired
+	private GroupeRepository groupeRepository;
 
-    @PostMapping("/")
-    public String createGroup(@RequestBody Groupe groupe) {
-        groupeRepository.save(groupe);
-        return "{ \"status\": \"Ok\" }";
-    }
+	@PostMapping("/")
+	public String createGroup(@RequestBody Groupe groupe) {
+		groupeRepository.save(groupe);
+		return "{ \"status\": \"Ok\" }";
+	}
 
-    @GetMapping("/{id}")
-    public Groupe getGroup(@PathVariable int id) {
-        return groupeRepository.getById(id);
-    }
+	@GetMapping("/{id}")
+	public Groupe getGroup(@PathVariable int id) {
+		return groupeRepository.getById(id);
+	}
 
-    @DeleteMapping("/{id}")
-    public String removeGroup(@PathVariable int id) {
-        groupeRepository.deleteById(id);
-        return "{ \"status\": \"Ok\" }";
-    }
+	@DeleteMapping("/{id}")
+	public String removeGroup(@PathVariable int id) {
+		groupeRepository.deleteById(id);
+		return "{ \"status\": \"Ok\" }";
+	}
 }
