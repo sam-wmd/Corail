5d6e1e2b7271336882d59652e712da173d42b7c1
Ronan <ronan.poinsignon@laposte.net>
2021-07-07 12:49:12 +0200

    suppression imports inutiles

--- CorailBackEnd/src/main/java/com/example/corailbackend/entity/group/Groupe.java
+++ CorailBackEnd/src/main/java/com/example/corailbackend/entity/group/Groupe.java
@@ -1,21 +1,23 @@
 package com.example.corailbackend.entity.group;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
 import com.example.corailbackend.entity.AbstractEntity;
 import com.example.corailbackend.entity.user.User;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Generated;
-
-import javax.persistence.*;
-import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 public class Groupe extends AbstractEntity {
 
-    private String nom;
+	private String nom;
 
-    @OneToMany
-    public Set<User> users;
+	@OneToMany
+	public Set<User> users;
 }
